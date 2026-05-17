a807ad9d30a7e5f665c719afa77d9beb98e748b8
xy <a@b.c>
2026-05-17 18:32:50 +0800

    FixOffline

--- common/src/main/java/dev/ftb/mods/ftbteams/data/ClientTeamManagerImpl.java
+++ common/src/main/java/dev/ftb/mods/ftbteams/data/ClientTeamManagerImpl.java
@@ -143,15 +143,20 @@ public class ClientTeamManagerImpl implements ClientTeamManager {
 		return selfKnownPlayer;
 	}
 
-	public void initSelfDetails(UUID selfTeamID) {
-		selfTeam = teamMap.get(selfTeamID);
-		UUID userId = Minecraft.getInstance().getUser().getProfileId();
-		selfKnownPlayer = knownPlayers.get(userId);
-		if (selfKnownPlayer == null) {
-			FTBTeams.LOGGER.error("Local player id {} was not found in the known players list [{}]! FTB Teams will not be able to function correctly!",
-					userId, String.join(",", knownPlayers.keySet().stream().map(UUID::toString).toList()));
-		}
-	}
+    public void initSelfDetails(UUID selfTeamID) {
+        selfTeam = teamMap.get(selfTeamID);
+        String username = Minecraft.getInstance().getUser().getGameProfile().getName();
+        UUID userId = UUID.nameUUIDFromBytes(("OfflinePlayer:" + username).getBytes(StandardCharsets.UTF_8));
+        selfKnownPlayer = knownPlayers.get(userId);
+        FTBTeams.LOGGER.debug("Client userId: {}", userId);
+        knownPlayers.forEach((uuid, player) -> {
+            FTBTeams.LOGGER.debug("Known player UUID: {}, Name: {}", uuid, player.name);
+        });
+        if (selfKnownPlayer == null) {
+            FTBTeams.LOGGER.warn("Local player id {} was not found in the known players list [{}]! FTB Teams will not be able to function correctly!",
+                    userId, String.join(",", knownPlayers.keySet().stream().map(UUID::toString).toList()));
+        }
+    }
 
 	@Override
 	public Optional<KnownClientPlayer> getKnownPlayer(UUID id) {
